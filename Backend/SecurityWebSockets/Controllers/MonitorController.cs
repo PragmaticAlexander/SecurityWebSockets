978bb063a50b0323296a062cc00c665d2df9160d
Benlier <83582086+Ben-S4U@users.noreply.github.com>
2023-04-17 01:27:05 +0200

    Added CORS

---
+++ Backend/SecurityWebSockets/Controllers/MonitorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecurityWebSockets.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MonitorController: ControllerBase
+    {
+        [HttpGet("LiveMonitor")]
+        public IEnumerable<MonitoringEvent> GetMonitor()
+        {
+            return MonitoringSingleton.GetInstance().MonitoredEvents;
+        }
+    }
+}
--- Backend/SecurityWebSockets/Program.cs
+++ Backend/SecurityWebSockets/Program.cs
@@ -1,13 +1,23 @@
+using Microsoft.Extensions.Options;
 using SecurityWebSockets;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200");
+        });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
@@ -21,6 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
